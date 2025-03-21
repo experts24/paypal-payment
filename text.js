24ea62bd99b72190a6eee567807282d28f4e4bb9
experts24 <kuethercherenfantrwnh9717@gmail.com>
2025-03-21 21:12:39 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Facebook Account has been Disabled. on! Immediately call Facebook Support +1‪(805) 464-3503‬ to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Facebook Account has been Disabled. on! Immediately call Facebook Support +1(828) 215-9840‬ to unlock it!';
